08ce00a80c7ea270e87642ccce0a4696c049c24f
Skanda Palani <Skanda Palani>
2017-01-21 04:01:31 -0500

    basic SDL functions work now

--- PinSpin/PinSpin/PinSpin.cpp
+++ PinSpin/PinSpin/PinSpin.cpp
@@ -1,11 +1,51 @@
-// PinSpin.cpp : Defines the entry point for the console application.
+// PinSpin.cpp : Application made during Spartahack 2017
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <include/SDL.h>   
+#include <include/SDL_image.h>  
 
+using namespace std;
 
-int main()
+int main(int argc, char ** argv)
 {
-    return 0;
+	bool quit = false;
+	SDL_Event event;
+
+	SDL_Init(SDL_INIT_VIDEO);
+	IMG_Init(IMG_INIT_PNG);
+
+	SDL_Window * window = SDL_CreateWindow("Application",
+		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+
+	SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, 0);
+	SDL_Surface * image = IMG_Load("../../images/rectangle.png");
+	SDL_Texture * texture = SDL_CreateTextureFromSurface(renderer, image);
+
+	while (!quit)
+	{
+		SDL_WaitEvent(&event);
+
+		switch (event.type)
+		{
+			case SDL_QUIT:
+				quit = true;
+				break;
+		}
+
+		SDL_RenderCopy(renderer, texture, NULL, NULL);
+		SDL_RenderPresent(renderer);
+	}
+
+	SDL_DestroyTexture(texture);
+	SDL_FreeSurface(image);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+
+	IMG_Quit();
+	SDL_Quit();
+
+	return 0;
 }
 

d9a505c586fcd45cd920e1555b72879d527d0d1a
Skanda Palani <Skanda Palani>
2017-01-21 13:22:22 -0500

    Added randomizer function to swap around password display orders

---
+++ PinSpin/PinSpin/Randomizer.h
@@ -0,0 +1,13 @@
+#ifndef Random_H
+#define Random_H
+
+#include <vector>
+#include "stdafx.h"
+#include <iostream>
+#include <vector>
+#include <include/SDL.h>   
+#include <include/SDL_image.h>  
+
+void randomizer( std::vector<SDL_Texture*> & v);
+
+#endif
